7330e98c38b848c3dd3864a8c96afb6f89d58005
uncsammy <khalid.omar@metropolia.fi>
2026-09-16 13:48:24 +0300

    Complete media API assignment

--- Assignment3/src/components/Home.jsx
+++ Assignment3/src/components/Home.jsx
@@ -4,12 +4,23 @@ import fetchData from '../utils/fetchData';
 
 const Home = () => {
   const [mediaArray, setMediaArray] = useState([]);
-
-    useEffect(() => {
+console.log(import.meta.env.VITE_MEDIA_API);
+  useEffect(() => {
     const getMedia = async () => {
       try {
-        const json = await fetchData('test.json');
-        setMediaArray(json);
+       const json = await fetchData(import.meta.env.VITE_MEDIA_API + '/media');
+
+const newArray = await Promise.all(
+  json.map(async (item) => {
+    const result = await fetchData(
+      import.meta.env.VITE_AUTH_API + '/users/' + item.user_id
+    );
+
+    return { ...item, username: result.username };
+  })
+);
+
+setMediaArray(newArray);
       } catch (error) {
         console.error('Error fetching media:', error);
       }
@@ -17,9 +28,7 @@ const Home = () => {
 
     getMedia();
   }, []);
-
-  console.log(mediaArray);
-
+console.log(mediaArray);
   return (
     <>
       <h2>Media</h2>
@@ -33,6 +42,7 @@ const Home = () => {
             <th>Created</th>
             <th>Filesize</th>
             <th>Media type</th>
+            <th>Username</th>
           </tr>
         </thead>
 
